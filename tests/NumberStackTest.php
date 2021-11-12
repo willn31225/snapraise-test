92959c98d53950b2e8f7ada57fc53f24f7e5b2f7
Will Noth <will.noth@mjfreeway.com>
2021-11-11 22:15:07 -0600

    Completed initial setup of PHPUnit

---
+++ tests/NumberStackTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class NumberStackTest extends TestCase
+{
+    public function testPHPUnit(): void
+    {
+        $this->assertTrue(true);
+    }
+}
